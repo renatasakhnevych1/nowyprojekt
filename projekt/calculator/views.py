9b94984482c5bc0ac5ce953ecca5cc5ef1599bd2
dawid.wolski <dawidspore@wp.pl>
2023-11-27 06:09:42 -0500

    nowe

--- projekt/calculator/views.py
+++ projekt/calculator/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 def hello(request, number):
     return HttpResponse(f"Hello, World! {number}")
 
+
+
+@csrf_exempt
 def calc(request):
-    pass
+    data = json.loads(request.body)
+    if data["operation"] == "+":
+        result = data["a"] + data["b"]
+    return HttpResponse(f"{result}")
